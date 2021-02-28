086129fc6a89655ccd69172af0390953508c8077
Evgenii Chernyshov <mailbox@eugesher.site>
2021-02-28 19:00:48 +0300

    feat: auth form component

---
+++ src/components/AuthForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function AuthForm({ children, optLink, name, title, submitButtonLabel }) {
+  AuthForm.propTypes = {
+    children: PropTypes.node,
+    optLink: PropTypes.node,
+    name: PropTypes.string,
+    title: PropTypes.string,
+    submitButtonLabel: PropTypes.string,
+  };
+
+  return (
+    <form name={name} noValidate className="form form_type_auth">
+      <div>
+        <h2 className="form__title form__title_type_auth">{title}</h2>
+        {children}
+      </div>
+      <div>
+        <button type="submit" className="form__submit-button form__submit-button_type_auth">
+          {submitButtonLabel}
+        </button>
+        <div className="form__opt-link-container">{optLink}</div>
+      </div>
+    </form>
+  );
+}
