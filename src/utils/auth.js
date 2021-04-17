a2298b206f25b2a39cb2e5519d1aa7c0a92cb0b4
Evgenii Chernyshov <mailbox@eugesher.site>
2021-04-17 18:50:12 +0300

    fix: api url

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.mesto.eugesher.site";
 
 export function register({ email, password }) {
   return fetch(`${BASE_URL}/signup`, {
